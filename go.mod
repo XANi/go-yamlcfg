159d5c202c44255724c42568146ff14a0db58cc7
Mariusz Gronczewski <xani666@gmail.com>
2023-11-11 18:47:54 +0100

    cleanup and upgrade deps

--- go.mod
+++ go.mod
@@ -1,7 +1,14 @@
 module github.com/XANi/go-yamlcfg
 
+go 1.21.3
+
 require (
-	github.com/smartystreets/goconvey v1.6.4
 	github.com/stretchr/testify v1.5.1
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- yamlcfg_test.go
+++ yamlcfg_test.go
@@ -2,22 +2,24 @@ package yamlcfg
 
 import (
 	"fmt"
-	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"os"
+	"path/filepath"
 	"regexp"
 	"testing"
-	"path/filepath"
-	"os"
 )
 
 type testCfg1 struct {
 	ConfigSource_ string
-	Test1 string `yaml:"test1"`
-	Test2 map[string]string `yaml:"test2"`
-	Test3 string
+	Test1         string            `yaml:"test1"`
+	Test2         map[string]string `yaml:"test2"`
+	Test3         string
+	Bool1         bool `yaml:"bool1"`
+	Bool2         bool `yaml:"bool2"`
 }
 
-func (c *testCfg1)SetConfigPath(s string) {
+func (c *testCfg1) SetConfigPath(s string) {
 	c.ConfigSource_ = s
 }
 
@@ -27,11 +29,13 @@ type testCfg2 struct {
 
 type testCfg3 struct {
 	Test3 string `yaml:"test3"`
-	path string
+	path  string
 }
+
 var testCfg3Default = `---
 test3: testing
 `
+
 func (c *testCfg3) GetDefaultConfig() string {
 	return testCfg3Default
 }
@@ -41,79 +45,71 @@ func (c *testCfg3) SetConfigPath(s string) {
 
 func TestService(t *testing.T) {
 	var c testCfg1
-	err := LoadConfig([]string{"./t-data/t1.cfg"}, &c)
-	Convey("LoadConfig", t, func() {
-		So(err, ShouldEqual, nil)
-		So(c.Test1,ShouldEqual,"aaa")
-		So(c.Test2["test3"],ShouldEqual,"123")
-		So(c.Test3,ShouldEqual,"bbb")
+	y := testCfg1{
+		ConfigSource_: "t-data/t1.cfg",
+		Test1:         "aaa",
+		Test2:         map[string]string{"test3": "123"},
+		Test3:         "bbb",
+		Bool1:         true,
+		Bool2:         true,
+	}
+	t.Run("load yaml", func(t *testing.T) {
+		err := LoadConfig([]string{"./t-data/t1.cfg"}, &c)
+		require.NoError(t, err)
+		assert.Contains(t, y.ConfigSource_, "t-data/t1.cfg")
+		y.ConfigSource_ = c.ConfigSource_
+		assert.Equal(t, y, c)
 	})
-	Convey("Show Config source", t, func() {
-		So(c.ConfigSource_,ShouldContainSubstring,"t-data/t1.cfg")
+	t.Run("nonexistent error", func(t *testing.T) {
+		err := LoadConfig([]string{"/nonexisting/file"}, &c)
+		assert.Error(t, err)
 	})
-	err = LoadConfig([]string{"/nonexisting/file"}, &c)
-	Convey("Error on no file found",t,func() {
-		So(err,ShouldNotEqual,nil)
+	t.Run("partial config", func(t *testing.T) {
+		var partial testCfg2
+		err := LoadConfig([]string{"./t-data/t1.cfg"}, &partial)
+		require.NoError(t, err)
+		assert.Equal(t, "aaa", partial.Test1)
 	})
-	var partial testCfg2
-	err = LoadConfig([]string{"./t-data/t1.cfg"}, &partial)
-	Convey("Load partial config", t, func() {
-		So(err, ShouldEqual, nil)
-		So(c.Test1,ShouldEqual,"aaa")
-	})
-	err = LoadConfig([]string{}, &c)
-	Convey("No config file in slice",t,func() {
-		So(err,ShouldNotBeNil)
+	t.Run("no config", func(t *testing.T) {
+		err := LoadConfig([]string{}, &c)
+		require.Error(t, err)
 	})
 }
 
 func TestDefaultGeneration(t *testing.T) {
 	var c testCfg3
 	tPath, err := filepath.Abs(os.ExpandEnv(`./t-data/t3.cfg.local`))
-	if err != nil {
-		Convey("test setup error - can't get path",t,func() {
-			So(err,ShouldEqual,nil)
-		})
-	}
+	require.NoError(t, err)
 	// ensure file does not exist
 	if _, err := os.Stat(tPath); err == nil {
 		err := os.Remove(tPath)
 		if err != nil {
-			Convey("test setup error - can't remove file",t,func() {
-				So(err,ShouldEqual,nil)
-			})
+			t.Fatal("test setup error - can't remove file")
 		}
 	}
 	err = LoadConfig([]string{"./t-data/t3.cfg.local"}, &c)
-	Convey("Create default config",t,func() {
-		So(err,ShouldEqual,nil)
-		So(c.path,ShouldContainSubstring,`/t-data/t3.cfg.local`)
-	})
-
-
+	assert.NoError(t, err)
+	assert.Contains(t, c.path, `/t-data/t3.cfg.local`)
 }
 
 func TestRandomString(t *testing.T) {
-	str1 := RandomString("abcd1",100)
+	str1 := RandomString("abcd1", 100)
 	abcdRe := regexp.MustCompile(`^[abcd1]+$`)
 	matched := abcdRe.MatchString(str1)
-	Convey("string should contain only abcd1: " + str1 , t, func () {
-		So(len(str1), ShouldEqual, 100)
-		So(matched,ShouldBeTrue)
-	})
+	assert.Len(t, str1, 100)
+	assert.True(t, matched)
 }
 
 type testCfgValidate struct {
 	Test1 string `yaml:"test1"`
 }
 
-func (c *testCfgValidate)Validate() error {
+func (c *testCfgValidate) Validate() error {
 	return fmt.Errorf("err out on validate")
 }
-
 
 func TestValidate(t *testing.T) {
 	var cfg testCfgValidate
 	err := LoadConfig([]string{"./t-data/t1.cfg"}, &cfg)
-	assert.EqualError(t,err,"err out on validate")
+	assert.EqualError(t, err, "err out on validate")
 }
