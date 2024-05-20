e5361f7134f72b2e673fa749a40646ca8ca2d5f2
Mariusz Gronczewski <xani666@gmail.com>
2024-05-20 14:56:30 +0200

    update modules

--- yamlcfg.go
+++ yamlcfg.go
@@ -1,23 +1,26 @@
 package yamlcfg
 
 import (
+	"crypto/rand"
 	"fmt"
 	"gopkg.in/yaml.v3"
-	"crypto/rand"
+	"io/ioutil"
 	"math/big"
 	"os"
-	"io/ioutil"
 	"path/filepath"
 )
 
 var CharsetAlphanumeric = "1234567890ABCDEFGHIKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz"
+
 // LoadConfig loads first YAML file on cfgFiles list into cfg interface
 // if cfg have defined GetDefaultConfig() method it will also create a default config file in first location if it can't find any config and the method returns more than zero characters
 // see README ( https://github.com/XANi/go-yamlcfg/blob/master/README.md ) for details
 func LoadConfig(cfgFiles []string, cfg interface{}) (err error) {
 	var cfgFile string
-	if len(cfgFiles) < 1 { return fmt.Errorf("cfgFiles slice needs at least one element") }
-	for _,element := range cfgFiles {
+	if len(cfgFiles) < 1 {
+		return fmt.Errorf("cfgFiles slice needs at least one element")
+	}
+	for _, element := range cfgFiles {
 		filename, _ := filepath.Abs(os.ExpandEnv(element))
 		if _, err := os.Stat(filename); err == nil {
 			cfgFile = filename
@@ -25,19 +28,19 @@ func LoadConfig(cfgFiles []string, cfg interface{}) (err error) {
 		}
 	}
 	if cfgFile == "" {
-		if cfg, ok := cfg.(interface{GetDefaultConfig() string}); ok {
+		if cfg, ok := cfg.(interface{ GetDefaultConfig() string }); ok {
 			defaultCfg := cfg.GetDefaultConfig()
 			if len(defaultCfg) == 0 {
 				return fmt.Errorf("could not find config file: %v", cfgFiles)
 			}
 			defaultCfgPath, _ := filepath.Abs(os.ExpandEnv(cfgFiles[0]))
-			err := os.MkdirAll(filepath.Dir(defaultCfgPath),os.FileMode(0755))
+			err := os.MkdirAll(filepath.Dir(defaultCfgPath), os.FileMode(0755))
 			if err != nil {
-				return fmt.Errorf("could not create directory; tried to create default config in [%s]: %s",defaultCfg,  err)
+				return fmt.Errorf("could not create directory; tried to create default config in [%s]: %s", defaultCfg, err)
 			}
-			err = ioutil.WriteFile(defaultCfgPath,[]byte(defaultCfg), os.FileMode(0600))
+			err = ioutil.WriteFile(defaultCfgPath, []byte(defaultCfg), os.FileMode(0600))
 			if err != nil {
-				return fmt.Errorf("could not find config file; tried to create default config in [%s]: %s",defaultCfg, err)
+				return fmt.Errorf("could not find config file; tried to create default config in [%s]: %s", defaultCfg, err)
 			} else {
 				cfgFile = defaultCfgPath
 			}
@@ -46,30 +49,39 @@ func LoadConfig(cfgFiles []string, cfg interface{}) (err error) {
 		}
 	}
 	raw_cfg, err := ioutil.ReadFile(cfgFile)
-	if err != nil { return err }
-	if len(raw_cfg) < 1 { return fmt.Errorf("Something gone wrong, file %s is 0 size or can't be read", cfgFile) }
+	if err != nil {
+		return err
+	}
+	if len(raw_cfg) < 1 {
+		return fmt.Errorf("Something gone wrong, file %s is 0 size or can't be read", cfgFile)
+	}
 	err = yaml.Unmarshal([]byte(raw_cfg), cfg)
-	if err != nil { return err }
-	if cfg, ok := cfg.(interface{SetConfigPath(string)}); ok {
+	if err != nil {
+		return err
+	}
+	if cfg, ok := cfg.(interface{ SetConfigPath(string) }); ok {
 		cfg.SetConfigPath(cfgFile)
 	}
-	if cfg, ok := cfg.(interface{Validate() error}); ok {
+	if cfg, ok := cfg.(interface{ Validate() error }); ok {
 		err := cfg.Validate()
-		if err != nil { return err }
+		if err != nil {
+			return err
+		}
 	}
-
-
 
 	return err
 }
+
 // RandomString generates random string with the defined dictionary
 func RandomString(chars string, length int) string {
 	b := make([]byte, length)
 	chLength := big.NewInt(int64(len(chars)))
 	for i := range b {
-		r, err := rand.Int(rand.Reader,chLength)
-		if err != nil {panic("error getting random data: " + err.Error())}
+		r, err := rand.Int(rand.Reader, chLength)
+		if err != nil {
+			panic("error getting random data: " + err.Error())
+		}
 		b[i] = chars[r.Int64()]
-  }
-  return string(b)
+	}
+	return string(b)
 }
